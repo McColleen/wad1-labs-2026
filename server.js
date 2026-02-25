3e9d9becc7c5177e11a6a9ffea8a684c322f3bd5
McColleen <colleenmccarthyie@outlook.com>
2026-02-25 12:39:22 +0000

    Add Models and Views

--- server.js
+++ server.js
@@ -1,39 +1,19 @@
 'use strict';
 
-import logger from './utils/logger.js';
 import express from 'express';
-import routes from './routes.js'; 
+import routes from "./routes.js";
+import logger from "./utils/logger.js";
+import { create } from 'express-handlebars';
 
 const app = express();
 const port = 3000;
+
+app.use(express.static("public"));
+
+const handlebars = create({extname: '.hbs'});
+app.engine(".hbs", handlebars.engine);
+app.set("view engine", ".hbs");
+
 app.use("/", routes);
 
-app.listen(port, () => console.log(`Express app running on port ${port}!`));
-
-app.get('/', (request, response) => response.send('Welcome to the Playlist app!'));
-app.get('/error', (request, response) => response.status(404).end('Page not found.'));
-app.get("/dashboard", (request, response) => {
-  const playlist = [
-    {
-      id: 1,
-      title: "Piano Sonata No. 3",
-      artist: "Beethoven"
-    },
-    {
-      id: 2,
-      title: "Piano Sonata No. 7",
-      artist: "Beethoven"
-    },
-    {
-      id: 3,
-      title: "Piano Sonata No. 10",
-      artist: "Beethoven"
-    }
-  ];
-  response.json(playlist);
-});
-
-
-app.listen(port, () => logger.info(`Your app is listening on port ${port}!`));
-
-
+app.listen(port, () => logger.info(`Your app is listening on port ${port}`));
