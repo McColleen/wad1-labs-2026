ad66974e104f40b3a405ee270fd4938967a33c6e
McColleen <colleenmccarthyie@outlook.com>
2026-05-06 10:21:53 +0100

    Cloudnairy Function

--- models/playlist-store.js
+++ models/playlist-store.js
@@ -14,8 +14,15 @@ const playlistStore = {
   getPlaylist(id) {
     return this.store.findOneBy(this.collection, (playlist => playlist.id === id));
   },
-  addPlaylist(playlist) {
-    this.store.addCollection(this.collection, playlist);
+  async addPlaylist(playlist, file, response) {
+    try {
+      playlist.picture = await this.store.addToCloudinary(file);
+      this.store.addCollection(this.collection, playlist);
+      response();
+    } catch (error) {
+      logger.error("Error processing playlist:", error);
+      response(error);
+    }
   },
   addSong(id, song) {
     this.store.addItem(this.collection, id, this.array, song);
@@ -23,9 +30,20 @@ const playlistStore = {
   removeSong(id, songId) {
     this.store.removeItem(this.collection, id, this.array, songId);
   },
-  removePlaylist(id) {
+  async removePlaylist(id, response) {
     const playlist = this.getPlaylist(id);
+
+    if (playlist.picture && playlist.picture.public_id) {
+      try {
+        await this.store.deleteFromCloudinary(playlist.picture.public_id);
+        logger.info("Cloudinary image deleted");
+      } catch (err) {
+        logger.error("Failed to delete Cloudinary image:", err);
+      }
+    }
+
     this.store.removeCollection(this.collection, playlist);
+    response();
   },
   editSong(id, songId, updatedSong) {
     this.store.editItem(this.collection, id, songId, this.array, updatedSong);
--- server.js
+++ server.js
@@ -4,39 +4,48 @@ import express from 'express';
 import routes from "./routes.js";
 import logger from "./utils/logger.js";
 import { create } from 'express-handlebars';
+import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
 
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
+
+app.use(bodyParser.urlencoded({ extended: false, }));
 app.use(cookieParser());
+app.use(fileUpload({useTempFiles: true}));
 
 const handlebars = create({
-	extname: '.hbs',
-	helpers: {
-		uppercase: (inputString) => {
-			return String(inputString).toUpperCase();
-		},
-		formatDate: (date) => {
-			const dateCreated = new Date(date);
-			const options = {
-				weekday: "long",
-				year: "numeric",
-				month: "long",
-				day: "2-digit",
-			};
-			return `${dateCreated.toLocaleDateString("en-IE", options)}`;
-		},
-			highlightPopular: (rating) => {
-				const message = rating >= 4 ? "Popular with listeners!" : "";
-				return message;
-			},
-	},
+    extname: '.hbs',
+    helpers: {
+
+        uppercase: (inputString) => {
+            return inputString.toUpperCase();
+        },
+
+        formatDate: (date) => {
+            let dateCreated = new Date(date);
+            let options = {
+                weekday: "long",
+                year: "numeric",
+                month: "long",
+                day: "2-digit",
+            };
+            return `${dateCreated.toLocaleDateString("en-IE", options)}`;
+        },
+
+        highlightPopular: (rating) => {
+            let message = rating >= 4 ? "Popular with listeners!" : "";
+            return message;
+        },
+
+    }
 });
 app.engine(".hbs", handlebars.engine);
 app.set("view engine", ".hbs");
+
 
 app.use("/", routes);
 
